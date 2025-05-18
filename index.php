6d90c864a2b36e636f94d045866b6fef1cab0a04
Rambabu Dhakad <dhakadram667@gamil.com>
2025-05-18 16:25:42 +0530

    footer

--- index.php
+++ index.php
@@ -789,9 +789,10 @@ if (!isset($_SESSION['email'])) {
                         <div class="position-relative">
                             <input class="form-control rounded-pill w-100 py-3 ps-4 pe-5" type="text"
                                 placeholder="Enter your email">
-                           
-                            <a  href="sing.html" class="btn btn-secondary rounded-pill position-absolute top-0 end-0 py-2 mt-2 me-2">Sign 
-                                In </a> 
+                        
+                                <a href="sing.html"><button type="button"
+                                class="btn btn-secondary rounded-pill position-absolute top-0 end-0 py-2 px-4 mt-2 me-2">Sign In</button></a>
+                
                         </div>
                     </div>
                 </div>
